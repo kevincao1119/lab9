4a0c08c9989dcd2898a4536bb8c01f1bf03a5cb8
kevincao1119 <kevin22@my.yorku.ca>
2018-11-27 20:18:27 +0000

    “setup”

--- countButtonPress.sh
+++ countButtonPress.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-# 4 is the buzzer
 
-for i in {1..16}
+# 0 RED, 1 BLUE, 2 RED, 3 BLUE, 4 BUZZER, 5 BUTTON
+
+a=0
+read=$(gpio read 5)
+
+while true
 do
-	if [ $i -eq "16" ]; then
-	./setbits.sh 0
-	gpio write 4 1
-	sleep 1
-	gpio write 4 0
-	
-	else
+    read=$(gpio read 5)
+    if [ "$read" -eq 0 ]
+    then
+        
+        ((a++))
 	./waitForButtonPress.sh
-	./setbits.sh $i
-	fi
+	echo "$a"
+        ./setbits.sh "$a"
+
+        if [ "$a" -eq 16 ]
+        then
+	gpio write 4 1 
+            sleep 1
+            gpio write 4 0      
+       break
+        fi
+
+        if [ "$a" -eq 0 ]
+        then
+            gpio write 4 1 
+            sleep 1
+            gpio write 4 0 
+        fi
+    fi
 done
-
-
-echo FINISH
--- waitForButtonPress.sh
+++ waitForButtonPress.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-# 0 means pressed, 1 means not pressed
 
-let button=$(gpio read 5)
-while test $button -eq 1;
-do 
-	echo wait
-	let button=$(gpio read 5)
+echo "waiting for button press"
+
+until [ $(gpio read 5) -eq 0 ]
+do
+	echo "button not pressed"
 	sleep 0.25
-    
 done
-while test $button -eq 0;
-        do      
-                
-                echo pressed
-                let button=$(gpio read 5)
-                sleep 0.25
-        done
 
-echo FINISH
+ until [ $(gpio read 5) -eq 1 ]
+  do
+        echo "button pressed"
+	sleep 0.25 		
+ done
